df23ee6a7665dfa54157f7fa77029e3fd829d414
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-12-18 15:25:11 +0900

    Front page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,7 @@ class HomeController extends Controller
     {
 //        $categories = \App\Category::all();
 //        flash('환영합니다.');
+//        $salesinfos = \App\SalesInfo::orderBy('created_at','desc')->take(4)->get();
 
         return view('home');
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,68 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.app_home')
 
 @section('content')
     <div class="front_page">
-        <img src="/img/banner.png" width="70%" height="10%">
+        <img src="/img/banner.png">
         <div class="front_page container">
             <div class="front_page row">
                 <div class="front_page col">
                     <p>등록된 정보</p>
-                    <img src="/img/information.png" width="40%">
+                    <img class="img1" src="/img/information.png">
                     <p class="num">94,953건</p>
                 </div>
                 <div class="front_page col">
                     <p>등록된 정보</p>
-                    <img src="/img/money.png" width="30%">
+                    <img class="img2" src="/img/money.png">
                     <p class="num">3,897,125건</p>
                 </div>
                 <div class="front_page col">
                     <p>등록된 정보</p>
-                    <img src="/img/man.png" width="30%">
+                    <img class="img3" src="/img/man.png">
                     <p class="num">2,084건</p>
                 </div>
             </div>
-            <hr>
+            <hr class="front_page line">
             <h4>실시간 견적 리스트</h4>
-            @php($salesinfos = App\SalesInfo::orderBy('created_at','desc')->take(4)->get())
+{{--            {{ app('request')->input('api/v1')[2] }}--}}
+{{--            {{$salesinfos[0]}}--}}
+            {{--{{route('api/v1/')}}--}}
+            @php($salesinfos = \App\SalesInfo::orderBy('created_at','desc')->take(4)->get())
             @if(count($salesinfos) < 4)
             @else
                 <div class="front_page row">
                     <div class="front_page col realtime">
-                        <a href="detail/{{$salesinfos[0]->id}}">
-                            <h4>{{$salesinfos[0]->BusinessName}}</h4>
-                            <p class="realtime_name">{{$salesinfos[0]->SP_name}}</p>
-                            <p>{{$salesinfos[0]->Category}}</p>
-                            <p>{{$salesinfos[0]->state}}</p>
-                        </a>
+                        <p class="realtime_bname">{{$salesinfos[0]->BusinessName}}</p>
+                        <p class="realtime_name">{{$salesinfos[0]->SP_name}}</p>
+                        <p>{{$salesinfos[0]->Category}}</p>
+                        <p>{{$salesinfos[0]->state}}</p>
                     </div>
                     <div class="front_page col realtime">
-                        <a href="detail/{{$salesinfos[1]->id}}">
-                            <h4>{{$salesinfos[1]->BusinessName}}</h4>
-                            <p class="realtime_name">{{$salesinfos[1]->SP_name}}</p>
-                            <p>{{$salesinfos[1]->Category}}</p>
-                            <p>{{$salesinfos[1]->state}}</p>
-                        </a>
+                        <p class="realtime_bname">{{$salesinfos[1]->BusinessName}}</p>
+                        <p class="realtime_name">{{$salesinfos[1]->SP_name}}</p>
+                        <p>{{$salesinfos[1]->Category}}</p>
+                        <p>{{$salesinfos[1]->state}}</p>
                     </div>
                 </div>
                 <div class="front_page row">
                     <div class="front_page col realtime">
-                        <a href="detail/{{$salesinfos[2]->id}}">
-                            <h4>{{$salesinfos[2]->BusinessName}}</h4>
-                            <p class="realtime_name">{{$salesinfos[2]->SP_name}}</p>
-                            <p>{{$salesinfos[2]->Category}}</p>
-                            <p>{{$salesinfos[2]->state}}</p>
-                        </a>
+                        <p class="realtime_bname">{{$salesinfos[2]->BusinessName}}</>
+                        <p class="realtime_name">{{$salesinfos[2]->SP_name}}</p>
+                        <p>{{$salesinfos[2]->Category}}</p>
+                        <p>{{$salesinfos[2]->state}}</p>
                     </div>
                     <div class="front_page col realtime">
-                        <a href="detail/{{$salesinfos[0]->id}}">
-                            <h4>{{$salesinfos[3]->BusinessName}}</h4>
-                            <p class="realtime_name">{{$salesinfos[3]->SP_name}}</p>
-                            <p>{{$salesinfos[3]->Category}}</p>
-                            <p>{{$salesinfos[3]->state}}</p>
-                        </a>
+                        <p class="realtime_bname">{{$salesinfos[3]->BusinessName}}</>
+                        <p class="realtime_name">{{$salesinfos[3]->SP_name}}</p>
+                        <p>{{$salesinfos[3]->Category}}</p>
+                        <p>{{$salesinfos[3]->state}}</p>
                     </div>
                 </div>
             @endif
+            <h4>커뮤니티</h4>
+
         </div>
     </div>
 @endsection
+
+<script>
+
+</script>
