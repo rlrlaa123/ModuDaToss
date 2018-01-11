42b5edd2df8b12f900567e02c62d190cb1b97dd3
rlrlaa123 <kimdonghyun3366@gmail.com>
2018-01-11 19:18:21 +0900

    Controllers & Navigation: add middleware to controllers & css

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -12,7 +12,11 @@ use DB;
 
 class MemberController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function mypage()
     {
         $id = auth()->user()->id;
